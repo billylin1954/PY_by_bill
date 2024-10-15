b1bf5bfd91d4fc1ddaa2093d1441eb39bd8e8d57
billylin1954 <132108254+billylin1954@users.noreply.github.com>
2024-10-14 21:49:33 -0400

    Delete PY directory

--- PY/first_django/hello/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def hello(request):
-    return HttpResponse("hello world")
-def greet(request,name):
-     return HttpResponse(f"hello,{name.capitalize()}")
